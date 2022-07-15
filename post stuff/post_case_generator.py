c65e55cc68ca6b5f5d6865a92ba4dd48201a5473
alex <alexjfuhr@gmail.com>
2022-07-14 21:41:37 -0400

    Updated scripts

--- post stuff/post_case_generator.py
+++ post stuff/post_case_generator.py
@@ -1,6 +1,9 @@
 wood_types = ["spruce", "birch", "jungle", "acacia", "dark_oak"]
 nether_wood_types = ["crimson", "warped"]
 
+left_side = "world.setBlockState(pos, ((PostCropSeeds) stack.getItem()).getCrop().with(PostCropBlock.TYPE, "
+right_side = ""
+
 spruce="spruce"
 
 for current_wood in wood_types:
@@ -12,7 +15,7 @@ for current_wood in wood_types:
         '\"stripped_' + current_wood + '_wood_post\"']
     for current_variant in variants:
         print(f"case {current_variant}:")
-        print(f"    world.setBlockState(pos.up(), this.withAge(0).with(TomatoPostCropBlock.TYPE, PostType.{current_variant[1:-1].upper()}));")
+        print(f"    world.setBlockState(pos, ((PostCropSeeds) stack.getItem()).getCrop().with(PostCropBlock.TYPE, PostType.{current_variant[1:-1].upper()}));")
         print(f"    break;")
 
 for current_wood in nether_wood_types:
@@ -24,5 +27,5 @@ for current_wood in nether_wood_types:
         '\"stripped_' + current_wood + '_hyphae_post\"']
     for current_variant in variants:
         print(f"case {current_variant}:")
-        print(f"    world.setBlockState(pos.up(), this.withAge(0).with(TomatoPostCropBlock.TYPE, PostType.{current_variant[1:-1].upper()}));")
+        print(f"    world.setBlockState(pos, ((PostCropSeeds) stack.getItem()).getCrop().with(PostCropBlock.TYPE, PostType.{current_variant[1:-1].upper()}));")
         print(f"    break;")
