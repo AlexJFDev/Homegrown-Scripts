a43fe289eccdd79bb93565a818f25876a69c1a48
alex <alexjfuhr@gmail.com>
2022-11-27 13:28:34 -0500

    predicateMaker changes

--- predicateMaker.py
+++ predicateMaker.py
@@ -1,4 +1,4 @@
-for xCoord in range(-2,3):
-    for yCoord in range(-2,3):
-        for zCoord in range(-2,3):
+for xCoord in range(1,6):
+    for yCoord in range(-5,6):
+        for zCoord in range(-5,6):
             print(f"BlockPredicate.matchingFluids(List.of(Fluids.LAVA, Fluids.FLOWING_LAVA), new BlockPos({xCoord}, {yCoord}, {zCoord})),")
